c7c5514fec42d50686282bee35d1cd23002c3629
Srivatsav Bendi <43092343+srivatsavbendi@users.noreply.github.com>
2023-02-18 20:26:27 -0500

    1.7
    new aesthetics, added buymeacoffee

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,15 +11,15 @@ class Header extends Component {
 
     render() { 
         return (
-            <div className="header1 sticky-top py-2 bg-dark text-white text-left row block">
+            <div className="header1 py-2 bg-dark text-white text-left row block">
                 <span className = "col h2 my-3 mx-5"><a className="text-white text-decoration-none" href="/">Ambifyy</a></span>
                 {/*<span className = "float-right mr-5"><a className="text-white" href="/login"><i className="material-symbols-outlined account">account_circle</i></a></span>
                    <p><Link to="/Ambience">Ambience</Link></p>*/}
                 <span className="col-2 ">
                     <div className="row px-5">
-                        <span className="col-4"><LinkedButton url="/" text={<i className="material-symbols-outlined">dark_mode</i>} /></span>
-                        <span className="col-4"><ExpandButton text={<i className="material-symbols-outlined">open_in_full</i>} /></span>
+                        <span className="col-4"><LinkedButton url="https://www.buymeacoffee.com/" text={<i className="material-symbols-outlined">local_cafe</i>} /></span>
                         <span className="col-4 displaybtn"><DisplayButton text={<i className="material-symbols-outlined">expand_more</i>} /></span>
+                        <span className="col-4"><ExpandButton text={<i className="material-symbols-outlined">open_in_full</i>} /></span>
                     </div>
                 </span>
             </div>
--- src/components/wall.jsx
+++ src/components/wall.jsx
@@ -9,23 +9,22 @@ class Wall extends Component {
             <div className = "row m-4">
                 <div className="col-4">
                     <Card bgPath="snowycozycabin.mov" title="Cozy Cabin In The Winter" author="TheBearThatsGolden"/>
-                    <Card bgPath="4.gif" title="Rainy Street Corner" author="UrAWizardHarry"/>
-                    <Card bgPath="13.gif" title="Cozy Spaceship" author="UrAWizardHarry"/>
+                    <Card bgPath="watershoes.mov" title="Standing In The Rain" author="UrAWizardHarry"/>
+                    <Card bgPath="girlstudying.mov" title="Cozy Studying" author="UrAWizardHarry"/>
                     <Card bgPath="10.gif" title="LoFi Bedroom" author="luxpae2"/>
                     <Card bgPath="10.gif" title="LoFi Bedroom" author="luxpae2"/>
-
                 </div>
                 <div className="col-4">
                     <Card bgPath="spaceastronautstaringoutofwindow.mov" title="Astronaut Gazing At The Stars" author="UrAWizardHarry"/>
-                    <Card bgPath="6.gif" title="Nighttime Traffic" author="akashboletum"/>
-                    <Card bgPath="7.gif" title="Cozy Cafe" author="akashboletum"/>
+                    <Card bgPath="girlonledge.mov" title="Sitting On A Ledge" author="akashboletum"/>
+                    <Card bgPath="rainleaves.mov" title="Cozy Cafe" author="akashboletum"/>
                     <Card bgPath="8.gif" title="Dystopian Metropolis" author="akashboletum"/>
                     <Card bgPath="14.gif" title="Suburban Neighborhood" author="UrAWizardHarry"/>
                 </div>
                 <div className="col-4">
                     <Card bgPath="snowycity.mov" title="Snowy City" author="UrAWizardHarry"/>
-                    <Card bgPath="11.gif" title="Winter City" author="luxpae2"/>
-                    <Card bgPath="12.gif" title="Street Isolation" author="luxpae2"/>
+                    <Card bgPath="darkalley.mov" title="Dark Alley At Night" author="luxpae2"/>
+                    <Card bgPath="floorleaves.mov" title="Street Isolation" author="luxpae2"/>
                     <Card bgPath="2.gif" title="Snowy Resort" author="earthypure"/>
                     <Card bgPath="ambify31.png" title="Snowy Resort" author="earthypure"/>
                 </div>
