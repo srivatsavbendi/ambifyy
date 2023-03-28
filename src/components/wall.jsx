0983f21aa7c794ed85e86605cd5059efa29f1962
Srivatsav Bendi <srivatsavbendi@gmail.com>
2023-03-27 20:00:11 -0400

    Update wall.jsx

--- src/components/wall.jsx
+++ src/components/wall.jsx
@@ -9,24 +9,24 @@ class Wall extends Component {
             <div className = "row m-4 mt-5">
                 <div className="row mt-3">
                     <div className="col-4 mt-5">
-                        <Card bgPath="paristrain.mp4" title="Train Through Paris " tag1="Landscape" tag2="City" tag3="Night"/>
                         <Card bgPath="watershoes.mov" title="Standing In The Rain" tag1="Animated" tag2="Rain" tag3="Sneakers"/>
-                        <Card bgPath="girlstudying.mov" title="Cozy Studying" tag1="Animated" tag2="Room" tag3="Chill"/>
-                        <Card bgPath="nightparking.mp4" title="Chill In The Car" tag1="Landscape" tag2="City" tag3="Night"/>
                         <Card bgPath="spaceastronautstaringoutofwindow.mov" title="Astronaut Gazing At The Stars" tag1="Animated" tag2="Space" tag3="Astronaut"/>
-                    </div>
-                    <div className="col-4 mt-5">
-                        <Card bgPath="darkalley.mov" title="Dark Alley At Night" tag1="Animated" tag2="Rain" tag3="Night"/>
-                        <Card bgPath="rainleaves.mp4" title="Rainfall On Leaves" tag1="Animated" tag2="Rain" tag3="Nature"/>
-                        <Card bgPath="snowycozycabin.mov" title="Cozy Cabin In The Winter" tag1="Animated" tag2="Snow" tag3="Cabin"/>
-                        <Card bgPath="snowytracks.mp4" title="Snowy Tracks" tag1="Landscape" tag2="Snow" tag3="City"/>
+                        <Card bgPath="paristrain.mp4" title="Train Through Paris " tag1="Landscape" tag2="City" tag3="Night"/>
+                        <Card bgPath="girlstudying.mov" title="Cozy Studying" tag1="Animated" tag2="Room" tag3="Chill"/>
                         <Card bgPath="europeandaylight.mp4" title="European Springs" tag1="Landscape" tag2="City" tag3="Day"/>
                     </div>
                     <div className="col-4 mt-5">
-                        <Card bgPath="snowycity.mov" title="Snowy City" tag1="Animated" tag2="Snow" tag3="Night"/>
-                        <Card bgPath="musicrecord.mp4" title="Spinning Records" tag1="Everyday" tag2="Room" tag3="Day"/>
-                        <Card bgPath="floorleaves.mov" title="Fall Rain" tag1="Landscape" tag2="Rain" tag3="Day"/>
+                        <Card bgPath="darkalley.mov" title="Dark Alley At Night" tag1="Animated" tag2="Rain" tag3="Night"/>
+                        <Card bgPath="nightparking.mp4" title="Chill In The Car" tag1="Landscape" tag2="City" tag3="Night"/>
+                        <Card bgPath="rainleaves.mp4" title="Rainfall On Leaves" tag1="Animated" tag2="Rain" tag3="Nature"/>
+                        <Card bgPath="snowycozycabin.mov" title="Cozy Cabin In The Winter" tag1="Animated" tag2="Snow" tag3="Cabin"/>
+                        <Card bgPath="snowytracks.mp4" title="Snowy Tracks" tag1="Landscape" tag2="Snow" tag3="City"/>
+                    </div>
+                    <div className="col-4 mt-5">
                         <Card bgPath="girlonledge.mov" title="Sitting On A Ledge" tag1="Animated" tag2="City" tag3="Girl"/>
+                        <Card bgPath="musicrecord.mp4" title="Spinning Records" tag1="Everyday" tag2="Room" tag3="Day"/>
+                        <Card bgPath="snowycity.mov" title="Snowy City" tag1="Animated" tag2="Snow" tag3="Night"/>
+                        <Card bgPath="floorleaves.mov" title="Fall Rain" tag1="Landscape" tag2="Rain" tag3="Day"/>
                         <Card bgPath="lushwaterfall.mp4" title="Lush Waterfall" tag1="Landscape" tag2="Nature" tag3="Day"/>
                     </div>
                 </div>
